b83bcffa962eb3f28bca791e72cbbc81e6d392e1
Akshay26953 <amkharat2695@gmail.com>
2022-08-28 00:58:38 +0530

    created header in chat section

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,34 @@
+import SearchOutlined from '@mui/icons-material/SearchOutlined';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import { Avatar, IconButton } from '@mui/material';
 import React from 'react';
 import './Chat.css';
+import MoreVert from '@mui/icons-material/MoreVert';
 
 function Chat() {
   return (
     <div className='chat'>
-        <h1>I am chat</h1>
+        <div className="header">
+          <Avatar/>
+          <div className="header_info">
+            <h3>Room Name</h3>
+            <p>Last seen at ....</p>
+          </div>
+          <div className="header_Right">
+            <IconButton>
+              <SearchOutlined/>
+            </IconButton>
+            <IconButton>
+              <AttachFileIcon/>
+            </IconButton>
+            <IconButton>
+              <MoreVert/>
+            </IconButton>
+          </div>
+        </div>
+        <div className="chat_body">
+          
+        </div>
     </div>
   )
 }
