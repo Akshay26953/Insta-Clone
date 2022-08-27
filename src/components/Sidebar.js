3f8fbf68e49f1b4abf054df5f5a69a5abf2c1363
Akshay26953 <amkharat2695@gmail.com>
2022-08-27 23:38:40 +0530

    Created search bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./Sidebar.css";
-import Avatar from '@mui/material/Avatar';
+import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import MessageIcon from "@mui/icons-material/Message";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="header">
         <div className="header_left">
-            <Avatar src="https://cdn.britannica.com/60/222660-050-064DBA89/Dwayne-Johnson-AKA-The-Rock-2019.jpg?w=400&h=300&c=crop"/>
+          <Avatar src="https://cdn.britannica.com/60/222660-050-064DBA89/Dwayne-Johnson-AKA-The-Rock-2019.jpg?w=400&h=300&c=crop" />
         </div>
         <div className="header_right">
           <IconButton>
@@ -23,6 +24,12 @@ function Sidebar() {
           <IconButton>
             <MoreVertIcon />
           </IconButton>
+        </div>
+      </div>
+      <div className="search">
+        <div className="searchContainer">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search or start new chat" name="searchBar" id="searchBar" />
         </div>
       </div>
     </div>
