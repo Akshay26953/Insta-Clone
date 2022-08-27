2d85e38ae3888dc460689d5ea0d64c3438190568
Akshay26953 <amkharat2695@gmail.com>
2022-08-27 21:12:21 +0530

    Initial commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Sidebar.css";
+import Avatar from '@mui/material/Avatar';
+import IconButton from "@mui/material/IconButton";
+import MessageIcon from "@mui/icons-material/Message";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="header">
+        <div className="header_left">
+            <Avatar src="https://cdn.britannica.com/60/222660-050-064DBA89/Dwayne-Johnson-AKA-The-Rock-2019.jpg?w=400&h=300&c=crop"/>
+        </div>
+        <div className="header_right">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <MessageIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC4uqHsH5eQyCXUYlHxhxT6DkzKlV5GtBU",
+//     authDomain: "whatsapp-4ddfc.firebaseapp.com",
+//     projectId: "whatsapp-4ddfc",
+//     storageBucket: "whatsapp-4ddfc.appspot.com",
+//     messagingSenderId: "609132838987",
+//     appId: "1:609132838987:web:87223afd14de1b523ccdce",
+//     measurementId: "G-83SM75H3BF"
+//   };
