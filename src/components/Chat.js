1ef80dcd06a48c52adfb10f88c5c443a03361236
Akshay26953 <amkharat2695@gmail.com>
2022-08-28 07:14:07 +0530

    Crated chat body in chat section

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,36 +1,45 @@
-import SearchOutlined from '@mui/icons-material/SearchOutlined';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import { Avatar, IconButton } from '@mui/material';
-import React from 'react';
-import './Chat.css';
-import MoreVert from '@mui/icons-material/MoreVert';
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import { Avatar, IconButton } from "@mui/material";
+import React from "react";
+import "./Chat.css";
+import MoreVert from "@mui/icons-material/MoreVert";
 
 function Chat() {
   return (
-    <div className='chat'>
-        <div className="header">
-          <Avatar/>
-          <div className="header_info">
-            <h3>Room Name</h3>
-            <p>Last seen at ....</p>
-          </div>
-          <div className="header_Right">
-            <IconButton>
-              <SearchOutlined/>
-            </IconButton>
-            <IconButton>
-              <AttachFileIcon/>
-            </IconButton>
-            <IconButton>
-              <MoreVert/>
-            </IconButton>
-          </div>
+    <div className="chat">
+      <div className="header">
+        <Avatar />
+        <div className="header_info">
+          <h3>Room Name</h3>
+          <p>Last seen at ....</p>
         </div>
-        <div className="chat_body">
-          
+        <div className="header_Right">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
         </div>
+      </div>
+      <div className="chat_body">
+        <p className="chat_message">
+          <span className="chat_name">Akshay</span>
+          This is msg
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat_message chat_rec">
+          <span className="chat_name">Akshay</span>
+          This is msg
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
